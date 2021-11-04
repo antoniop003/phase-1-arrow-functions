3e642e742e2509556abd121e6f039acd60ee5465
Antonio Ruben Paitno <antonio.patino003@gmail.com>
2021-11-03 21:14:24 -0500

    commit tree 144893f48189dd784752465a0c669d0b3bcc1489

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(num1 = 2000,num2 = 100){
+return num1/num2
+}
+
+const square = x =>{
+    let maybe = x * x
+return maybe
+}
+
+const add = (a , b) => {
+    let poss = a + b
+    return poss
+}
